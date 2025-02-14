3b24f0d80362cfa7117d6bf7b1e427f059665cc9
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-14 13:03:59 -0600

    Update version to 2025.2.14-1303

--- dist/id/Ids.d.ts
+++ dist/id/Ids.d.ts
@@ -1,14 +1,27 @@
-export declare const from: (type?: string, unique?: `${string}-${string}-${string}-${string}-${string}`, timestamp?: string, separator?: string) => string;
+import type { ObjectStore } from "../DataConnectionMap";
+export declare const from: (type?: string, namespace?: string, unique?: `${string}-${string}-${string}-${string}-${string}`, timestamp?: string, separator?: string) => string;
+export declare const fromObjectStore: (objectStore: ObjectStore) => string;
 export declare const parse: (id: string, separator?: string) => {
+    namespace: string;
     type: string;
     timestamp: string;
     unique: string;
 } | undefined;
+export declare const toObjectStore: (id: string) => {
+    namespace: string;
+    store: string;
+} | undefined;
 export declare const Ids: {
-    from: (type?: string, unique?: `${string}-${string}-${string}-${string}-${string}`, timestamp?: string, separator?: string) => string;
+    from: (type?: string, namespace?: string, unique?: `${string}-${string}-${string}-${string}-${string}`, timestamp?: string, separator?: string) => string;
     parse: (id: string, separator?: string) => {
+        namespace: string;
         type: string;
         timestamp: string;
         unique: string;
+    } | undefined;
+    fromObjectStore: (objectStore: ObjectStore) => string;
+    toObjectStore: (id: string) => {
+        namespace: string;
+        store: string;
     } | undefined;
 };
--- dist/id/Ids.js
+++ dist/id/Ids.js
@@ -1,16 +1,28 @@
-export const from = (type = "id", unique = crypto.randomUUID(), timestamp = `${Date.now()}`, separator = ":") => {
-    return `${type}${separator}${timestamp}${separator}${unique}`;
+export const from = (type = "", namespace = "", unique = crypto.randomUUID(), timestamp = `${Date.now()}`, separator = ":") => {
+    return `${namespace}${separator}${type}${separator}${timestamp}${separator}${unique}`;
+};
+export const fromObjectStore = (objectStore) => {
+    const { namespace, store } = objectStore;
+    return from(store, namespace);
 };
 export const parse = (id, separator = ":") => {
     const regex = new RegExp(`^(.+)${separator}(\\d+)${separator}(.+)$`);
     const match = id.match(regex);
     if (match) {
-        const [_, type, timestamp, unique] = match;
-        return { type, timestamp, unique };
+        const [_, namespace, type, timestamp, unique] = match;
+        return { namespace, type, timestamp, unique };
     }
     else {
         return undefined;
     }
 };
-export const Ids = { from, parse };
+export const toObjectStore = (id) => {
+    const parsed = parse(id);
+    if (!parsed) {
+        return undefined;
+    }
+    const { namespace, type } = parsed;
+    return { namespace, store: type };
+};
+export const Ids = { from, parse, fromObjectStore, toObjectStore };
 //# sourceMappingURL=Ids.js.map
