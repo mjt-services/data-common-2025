ae252cf0a3e585a31880f26571b26b283edd0593
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-23 11:37:42 -0600

    add DATA_EVENT_MAP and UpdteSubject

---
+++ src/event/DATA_EVENT_MAP.ts
@@ -0,0 +1,6 @@
+import type { EventMap } from "@mjt-engine/message";
+import { type UpdateSubject } from "./UpdateSubject";
+
+export const DATA_EVENT_MAP: EventMap<UpdateSubject> = {
+  update: undefined,
+};
---
+++ src/event/UpdateSubject.ts
@@ -0,0 +1,3 @@
+import type { PartialSubject } from "@mjt-engine/message";
+
+export type UpdateSubject = PartialSubject<"update">;
