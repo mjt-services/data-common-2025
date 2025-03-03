09937929cf5218c682a7c9d01afd9eac40a377f5
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-03 13:26:55 -0600

    add child and object update events

--- src/event/DATA_EVENT_MAP.ts
+++ src/event/DATA_EVENT_MAP.ts
@@ -1,6 +1,14 @@
 import type { EventMap } from "@mjt-engine/message";
-import { type UpdateSubject } from "./UpdateSubject";
+import {
+  type ChildUpdateSubject,
+  type ObjectUpdateSubject,
+  type UpdateSubject,
+} from "./UpdateSubject";
 
-export const DATA_EVENT_MAP: EventMap<UpdateSubject> = {
+export const DATA_EVENT_MAP: EventMap<
+  UpdateSubject | ObjectUpdateSubject | ChildUpdateSubject
+> = {
   update: undefined,
+  object_update: undefined,
+  child_update: undefined,
 };
--- src/event/UpdateSubject.ts
+++ src/event/UpdateSubject.ts
@@ -1,3 +1,5 @@
 import type { PartialSubject } from "@mjt-engine/message";
 
 export type UpdateSubject = PartialSubject<"update">;
+export type ObjectUpdateSubject = PartialSubject<"object_update">;
+export type ChildUpdateSubject = PartialSubject<"child_update">;
